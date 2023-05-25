46fcff1fc2604e126a0e38f81588173916bd4d74
salarissa321 <salarissa321@gmail.com>
2023-05-25 21:36:23 +0200

    add Function-Return-Python

---
+++ Function-Return-Python.py
@@ -0,0 +1,35 @@
+
+
+
+#-----------------------------------------------------
+#--------- Function And Return-----------------
+#-----------------------------------------------------
+# [1] A Function is A Reusable Block of Code Do A Task
+# [2] A Function Run When You Call it
+# [3] A Function Accept Element To Deal With Called[Parameters]
+# [4] A Function Can Do The Task Without Returning Data
+# [5] A Function Can Return Data After Job is Finished
+# [6] A Function Create To Preven DRY
+# [7] A Function Accept Elements when You Called it [Arguments]
+# [8] There Is A Built-In Functions and User Defined Functions
+# [9] A Function is for All Team And All Apps
+#--------------------------------------------------------
+
+
+# Def function
+
+
+def function_name() :
+    print("Hello Python Form Inside Function ") 
+
+function_name() # Hello Python Form Inside Function 
+
+
+
+
+# return
+
+def function_name() :
+    return "Hello Python From Inside Function"
+
+print(function_name()) # Hello Python From Inside Function
